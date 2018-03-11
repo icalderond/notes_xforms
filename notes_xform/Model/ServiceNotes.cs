836e74020561d98a66e635fc4f2c65386ec7b726
icalderond <icalderond@outlook.com>
2018-03-11 16:19:03 -0500

    Mostrar lista de tareas en pantalla

--- notes_xform/Model/ServiceNotes.cs
+++ notes_xform/Model/ServiceNotes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace notes_xform.Model
 {
@@ -12,6 +13,21 @@ namespace notes_xform.Model
         public void getListNotes()
         {
             var listNotes = new List<Note>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                listNotes.Add(new Note()
+                {
+                    Title = $"Este es el Titulo numero {i}",
+                    Content = "Esto es un texto que es largo pero no tan larco como para cort...",
+                    Labels = new ObservableCollection<string>()
+                    {
+                        "#Israel",
+                        "Coliflor"
+                    }
+                });
+            }
+
             GetListNotes_Completed?.Invoke(this, new GenericEventArgs<List<Note>>(listNotes));
         }
     }
--- notes_xform/ViewModels/ListNotesViewModel.cs
+++ notes_xform/ViewModels/ListNotesViewModel.cs
@@ -10,6 +10,13 @@ namespace notes_xform.ViewModels
         public ListNotesViewModel()
         {
             serviceNotes = new ServiceNotes();
+
+            serviceNotes.GetListNotes_Completed += (s, a) =>
+            {
+                ListNotes = new ObservableCollection<Note>(a.Result);
+            };
+
+            serviceNotes.getListNotes();
         }
         private ObservableCollection<Note> _ListNotes;
         public ObservableCollection<Note> ListNotes
