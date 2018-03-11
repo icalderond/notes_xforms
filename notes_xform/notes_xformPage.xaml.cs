7a5d21097fc1cf97e804026b808d21075b0079d0
icalderond <icalderond@outlook.com>
2018-03-11 14:08:48 -0500

    Primer commit

---
+++ notes_xform/notes_xformPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace notes_xform
+{
+    public partial class notes_xformPage : ContentPage
+    {
+        public notes_xformPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
