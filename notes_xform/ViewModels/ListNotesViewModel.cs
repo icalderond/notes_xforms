3c3f50d878e089a2e7d6964c92c8ac47a6e47c0b
icalderond <icalderond@outlook.com>
2018-03-14 16:37:12 -0500

    Agregamdo Navegacion en VM con prism

--- notes_xform/ViewModels/ListNotesViewModel.cs
+++ notes_xform/ViewModels/ListNotesViewModel.cs
@@ -2,29 +2,33 @@
 using notes_xform.Model;
 using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 
 namespace notes_xform.ViewModels
 {
     public class ListNotesViewModel : BindableBase
     {
+        INavigationService _navigationService;
         ServiceNotes serviceNotes;
-        public ListNotesViewModel()
+
+        public ListNotesViewModel(INavigationService navigationService) // has to be named correctly
         {
+            _navigationService = navigationService;
+            //public ListNotesViewModel()
+            //{
             serviceNotes = new ServiceNotes();
 
-            serviceNotes.GetListNotes_Completed += (s, a) =>
-            {
-                ListNotes = new ObservableCollection<Note>(a.Result);
-            };
+            serviceNotes.GetListNotes_Completed += (s, a) => ListNotes = new ObservableCollection<Note>(a.Result);
 
             SelectedChangedCommand = new DelegateCommand<object>(SelectedChanged);
 
             serviceNotes.GetListNotes();
         }
+        public ListNotesViewModel() { }
 
         private void SelectedChanged(object param)
         {
-            var u = param;
+            _navigationService.NavigateAsync("");
         }
 
         private Note _NoteSelected;
